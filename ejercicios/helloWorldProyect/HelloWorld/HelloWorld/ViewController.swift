fcfa42a801f685bc16999cad813363a0cb16885f
markmota <marduk.p@gmail.com>
2019-08-16 06:37:37 -0500

    Ejercicio primera clase

---
+++ ejercicios/helloWorldProyect/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by markmota on 8/9/19.
+//  Copyright © 2019 markmota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
