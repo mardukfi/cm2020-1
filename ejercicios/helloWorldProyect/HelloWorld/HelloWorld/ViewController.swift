766e3b2b5399191a1099fba57b5e2a2f236c3c66
markmota <marduk.p@gmail.com>
2019-08-16 19:33:07 -0500

    Ejercicio 16 Agosto

--- ejercicios/helloWorldProyect/HelloWorld/HelloWorld/ViewController.swift
+++ ejercicios/helloWorldProyect/HelloWorld/HelloWorld/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func unwindToPortada(unwindSegue: UIStoryboardSegue){
+    }
 
 }
 
